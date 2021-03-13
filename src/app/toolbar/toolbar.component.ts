47b3a616ddcbb4310308fa7a99c73cc45351f636
Daniel del Salto Cobo <daniel11ds@hotmail.com>
2021-03-13 16:01:50 +0100

    Cambios visuales

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
+  rojo: number
+  verde: number
+  azul: number
 
-  constructor() { }
+  constructor() {  }
 
   ngOnInit(): void {
+    this.rojo = 255
+    this.verde = 0
+    this.azul = 0
+    this.arcoiris()
+  }
+
+  async arcoiris() {
+    const timer = ms => new Promise(res => setTimeout(res, ms))
+    while (true) {
+        for (let index = 255; index >= 0; index--) {
+          this.rojo--
+          this.verde++
+          await timer(50)
+        }
+        for (let index = 255; index >= 0; index--) {
+          this.verde--
+          this.azul++
+          await timer(50)
+        }
+        for (let index = 255; index >= 0; index--) {
+          this.azul--
+          this.rojo++
+          await timer(50)
+        }
+    }
   }
 
 }
